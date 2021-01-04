21ae19a18d6ba27362b7f39f4cc51d02efe20dab
IsaacCorley <isaac.a.corley@gmail.com>
2021-01-03 20:06:21 -0600

    update

--- torch_pointnet/__init__.py
+++ torch_pointnet/__init__.py
@@ -2,3 +2,4 @@ from . import datasets
 from . import models
 from . import transforms
 from . import losses
+from . import utils
---
+++ torch_pointnet/utils.py
@@ -0,0 +1,4 @@
+import open3d
+
+def visualize(x):
+    open3d.visualization.draw_geometries([x])
